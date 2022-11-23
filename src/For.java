dd9e115dff6b94fe5aee9c521203c85d006374eb
Adrian <a.razniak.9@gmail.com>
2022-11-23 20:32:27 +0100

    While 3 cases

--- src/For.java
+++ src/For.java
@@ -1,9 +1,10 @@
 public class For {
-//    public static void main(String[] args) {
-//        String[] cats=new String[]{"Burek","Mruczek","Iskierka","Pussy"};
-//        for (int i=0;i< cats.length;i++){
-//            System.out.println(cats[i]);
-//        }
-//
+    public static void main(String[] args) {
+       String[] cats=new String[]{"Burek","Mruczek","Iskierka","Pussy"};
+        for (int i=0;i< cats.length;i++){
+            System.out.println(cats[i]);
+        }
+}
+}
 
-//    }
+
